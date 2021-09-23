aeddde636e6d9da01d0e185276a0f7fc18913e98
rakesh2025 <rakesh.12345pradhan12345@gmail.com>
2021-09-23 14:53:14 +0530

    COMMIT .

--- ComcastSeleniumFrm/src/test/java/com/comcast2/OrganizationTest.java
+++ ComcastSeleniumFrm/src/test/java/com/comcast2/OrganizationTest.java
@@ -6,6 +6,11 @@ public class OrganizationTest {
 	@Test(groups = "SmokeSuite")
 	public void createOrgTest() {
 		System.out.println("createOrgTest");
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		
 	}
 	@Test(groups = "SmokeSuite")
 	public void createOrgWithContactTest() {
@@ -14,6 +19,10 @@ public class OrganizationTest {
 	@Test(groups = "RegressionSuite")
 	public void deleteOrgtTest() {
 		System.out.println("deleteOrgtTest");
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		System.out.println(BROWSER);
+		System.out.println(URL);
 	}
 
 }
